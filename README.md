adf55421569fe9f944e930be3708a94b8349c175
Rafael Flores Souza <Raflso@icloud.com>
2022-09-25 21:09:52 -0600

    Starting files

---
+++ README.md
@@ -0,0 +1,24 @@
+# SENG 513 - API
+
+## System Requirements
+>  </br>
+
+## Installation
+1. 
+
+## Usage
+</br>
+
+\[All\] </br>
+1. </br>
+
+
+## Version History
+*v1.0* <br/>
+</br>
+</br>
+
+
+## Contributors and Credit
+Richard Gingrich </br>
+Rafael Flores Souza </br>
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express"); 
+const mongoose = require("mongoose"); 
+const dotenv = require('dotenv');
+const path = require('path');
+
+// DotEnv Configuration
+dotenv.config();
+
+// Mongo DB Configuration
+const dbConfig = {
+    database : process.env.MONGO_DB ?? '',
+    hostname : process.env.MONGO_HOST ?? '',
+    port : process.env.MONGO_PORT ?? '',
+};
+
+const connectionURI = `mongodb://${dbConfig.hostname}:${dbConfig.port}/${dbConfig.database}`
+console.log(connectionURI)
+mongoose.connect(connectionURI);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+// App configuration
+const appConfig = { port : process.env.APP_PORT ?? '3000' }
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route Configuration
+
+// Starting the node.js server
+app.listen(appConfig.port, () => {
+    console.log("Serving on port 3000");
+});
