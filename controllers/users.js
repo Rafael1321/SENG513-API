fe8d6079a18b733d5e1cda5b44a6fc7a60dbac79
Rafael Flores Souza <Raflso@icloud.com>
2022-10-03 15:40:01 -0600

    Finished login user login endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,10 @@
+const bcrypt = require('bcrypt');
 const user = require("../models/user");
 
 module.exports.loginUser = (req, res) => {
 
     try{
-        const username = req.body.username;
+        const {username, password} = req.body;
 
         if(username == undefined || username === '' || username === null){
             return res.type('json').status(404).send({msg:'The username is missing'});   
@@ -13,20 +14,21 @@ module.exports.loginUser = (req, res) => {
         }
     
         // Check if the username exists
-        user.findOne({username:username}, (err, user) => {
-            if (err) return res.type('json').status(404).send({msg:"Username was not found."});       
-            else{
-                // Check if the passwords match
-                const isPasswordValid = bcrypt.compare(password, user.password);
-                if(!isPasswordValid){
-                    return res.type('json').status(404).send({msg:"The password is incorrect."});   
-                }
-            }
-        });
+        var searchedUser = null;
+        user.findOne({username:username}, (err, user) => { searchedUser = user; });
 
-        res.type('json').status(200).send(user);       
-    
+        if (searchedUser === null){             
+            return res.type('json').status(404).send({msg:"User was not found."});  
+        }else{
+            // Check if the passwords match
+            const isPasswordValid = bcrypt.compare(password, user.password);
+            if(!isPasswordValid){
+                return res.type('json').status(404).send({msg:"The password is incorrect."});   
+            }
+            return res.type('json').status(200).send({"data":user}); 
+        }
+       
     }catch(err) {
-        res.type('json').status(500).send();   
+        return res.type('json').status(500).send({msg:err.toString()});   
     }
 };
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ db.once("open", () => {
 const appConfig = { port : process.env.APP_PORT ?? '3000' }
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Route Configuration
