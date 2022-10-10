818f26baff57ca82c3469797ba746f6b3d770437
Rafael Flores Souza <Raflso@icloud.com>
2022-10-10 14:09:45 -0600

    Finished main functionality of register end-point

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
-// Controllers
 const users = require("../controllers/users");
 
-// prettier-ignore
+router.route('/users/register')
+      .post(users.registerUser);
+
 router.route('/users/login')
       .post(users.loginUser);
 
