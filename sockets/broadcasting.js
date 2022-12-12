ca6ab07c120c3ee027b613c15aa745982fb8ada8
Rafael Flores Souza <Raflso@icloud.com>
2022-12-11 22:57:24 -0700

    Fixed various issues with socket setup

--- sockets/broadcasting.js
+++ sockets/broadcasting.js
@@ -6,53 +6,61 @@ function broadcasting(io){ // For connection and disconnection
 
     io.on("connection", (socket) => {
 
-        const connectedUserId = socket.handshake.query['userId'];
-        if(!connectedUserId) return; 
-    
-        // Add new connected user to Map
-        if(!connectedUsers.has(connectedUserId)){
-            connectedUsers.set(connectedUserId, {socket:socket, online:false});
-        }
-    
+        socket.on('user_connected', (connectedUserId) => {
+            
+            if(!connectedUserId){
+                socket.emit(`error_user_connected`, {msg:`Invalid connected user id ${connectedUserId}.`});
+                return; 
+            }
+            
+            // Add new connected user to Map
+            if(!connectedUsers.has(connectedUserId)){
+                connectedUsers.set(connectedUserId, {socket:socket, online:false});
+                socket.emit(`success_user_connected`, {msg:`User with id ${connectedUserId} is set as connected.`});
+            }else{
+                socket.emit(`error_user_connected`, {msg:`User with id ${connectedUserId} is already connected.`});
+            }
+        });
+
         /* MATCHING */
 
         socket.on('find_matching', (userId) => {
- 
+            
             // In case user id is invalid.
-            if(!user){
-                socket.emit('error_find_matching','User id is invalid.');
+            if(!userId){
+                socket.emit('error_find_matching',{msg:'User id is invalid.'});
                 return;
             }
 
             // In case user id is not found
             if(!connectedUsers.has(userId)){
-                socket.emit('error_find_matching', 'User id does not exist.');
+                socket.emit('error_find_matching', {msg:'User id does not exist.'});
                 return;
             }
 
             // Set the user as "online"
             connectedUsers.set(userId, {...connectedUsers.get(userId), online:true});
 
-            socket.emit('success_find_matching');
+            socket.emit('success_find_matching', {msg:'User succesfully marked as online.'});
         });
 
         socket.on('stop_matching', (userId) => {
             // In case user id is invalid.
             if(!user){
-                socket.emit('error_stop_matching','User id is invalid.');
+                socket.emit('error_stop_matching',{msg:'User id is invalid.'});
                 return;
             }
 
             // In case user id is not found
             if(!connectedUsers.has(userId)){
-                socket.emit('error_stop_matching', 'User id does not exist.');
+                socket.emit('error_stop_matching', {msg:'User id does not exist.'});
                 return;
             }
 
             // Set the user as "offline"
             connectedUsers.set(userId, {...connectedUsers.get(userId), online:false});
 
-            socket.emit('success_stop_matching');
+            socket.emit('success_stop_matching', {msg:'User succesfully marked as offline.'});
         });
 
         socket.on('match_found', (myId, matchedWithId) => {
@@ -60,18 +68,18 @@ function broadcasting(io){ // For connection and disconnection
             // Invalid matched with id.
             let invalid = true;
             if(!matchedWithId){
-                socket.emit('error_match_found', 'Matched with id is invalid.')
+                socket.emit('error_match_found', {msg:'Matched with id is invalid.'})
             }else if(!connectedUsers.has(matchedWithId)){ // In case matched with id is not found
-                socket.emit('error_match_found', 'Matched with id does not exist.');
+                socket.emit('error_match_found', {msg:'Matched with id does not exist.'});
             }else if(!connectedUsers.get(matchedWithId).online){ // In case taht user is offline
-                socket.emit('error_match_found', 'Matched with id belong to an OFFLINE user.');
+                socket.emit('error_match_found', {msg:'Matched with id belong to an OFFLINE user.'});
             }else{
                 invalid = false;
             }
 
             if(invalid) return;
 
-            connectedUsers.get(matchedWithId).socket.emit('match_found', myId);
+            connectedUsers.get(matchedWithId).socket.emit('match_found', {matchedWithId:myId});
 
             socket.emit('success_match_found');
         });
@@ -81,7 +89,7 @@ function broadcasting(io){ // For connection and disconnection
         /* DISCONNECT*/
         io.on("disconnect", (socket) => {
     
-            const connectedUserId = socket.handshake.query['userId'];
+            const connectedUserId = getUserIdFromSocketId(socket.id);
             if(!connectedUserId) return; 
         
             // Remove user from map 
@@ -93,6 +101,17 @@ function broadcasting(io){ // For connection and disconnection
 }
 
 /* HELPER FUNCTIONS */
+
+function getUserIdFromSocketId(socketId){
+    let userId = "";
+    for (let [uId, info] of connectedUsers) {
+        if(info.socket.id === socketId){
+            userId = uId;
+            break;
+        }
+    }
+    return userId;
+}
 
 function getOnlineUserIds(){
     const onlineUsers = [];
