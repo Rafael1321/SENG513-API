21c0943a43e42daa3e355792de6c95454c46b120
Rafael Flores Souza <Raflso@icloud.com>
2022-12-14 17:39:39 -0700

    Fixed wrong routes

--- routes/chats.js
+++ routes/chats.js
@@ -3,10 +3,10 @@ const router = express.Router({ mergeParams: true });
 
 const chats = require("../controllers/chats");
 
-router.route('/chats')
+router.route('/chats/retrieve')
       .post(chats.retrieveMessages);
 
-router.route('/chats')
+router.route('/chats/save')
       .post(chats.saveMessage);
 
 module.exports = router;
